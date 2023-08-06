1d4d9f1f031bfb615fa8befe0e1f2a8830336d21
Dawid Kmak <dkmak02@gmail.com>
2023-08-06 10:23:33 +0200

    Deleted dapper

--- XYZ/Endpoints/LogIn.cs
+++ XYZ/Endpoints/LogIn.cs
@@ -1,16 +1,13 @@
 ﻿using FastEndpoints;
 using XYZ.Endpoints.Requests;
 using XYZ.Services.Auth;
-using XYZ.Services.Dapper;
 
 namespace XYZ.Endpoints
 {
     public class LogIn : Endpoint<LogInRequest>
     {
-        private readonly IDapper _dapper;
         private readonly IAuthService _authService;
-        public LogIn(IDapper dapper, IAuthService authService) { 
-            _dapper = dapper;
+        public LogIn(IAuthService authService) { 
             _authService = authService;
         }
         public override void Configure()
